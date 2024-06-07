6ebdee23c57159e5b6e9c7f67f823d2d434cc87f
Jathin Pranav Singaraju <pran.singaraju@gmail.com>
2024-06-07 01:54:46 -0500

    updated homepage + working on layout

--- gig-project/app/(home)/layout.js
+++ gig-project/app/(home)/layout.js
@@ -14,57 +14,68 @@ export default function HomeLayout({ children }) {
     <html className="h-full w-full bg-off-white">
       <body className="flex flex-col min-h-full">
         <nav className="sticky top-0 bg-off-white">
-          <div className="flex max-w-screen-5xl pl-10 pr-10 pt-4 pb-2 border-b-2 border-light-gray items-center">
-            <div className="flex items-center">
-              <Link href="/">
-                <img className="w-48" src="/name.png" alt="Company Logo" />
-              </Link>
+          <div className="flex max-w-screen-5xl pl-10 pr-10 pt-4 pb-2 border-b-2 border-light-gray items-center justify-between">
+            <div className="flex flex-row">
+              <div className="flex items-center">
+                <Link href="/">
+                  <img
+                    className="flex max-w-32"
+                    src="/name.png"
+                    alt="Company Logo"
+                  />
+                </Link>
+              </div>
+              <div className="group transition w-20 flex flex-row justify-start ml-2 mt-2">
+                <div className="text-gray-800 duration-3000 hover:text-mid-purple/80 font-normal hover:font-semibold transition-all">
+                  <Link
+                    href="/home"
+                    className="pt-5 pb-4 pl-6 font-poppins md:text-lg text-sm"
+                  >
+                    Home
+                    <span className="ml-6 block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 rounded-xl bg-mid-purple/80"></span>
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className="w-full flex flex-row justify-start ml-2">
-              <Link
-                href="/home"
-                className="pt-4 pb-4 pl-6 font-poppins text-dark-gray md:text-lg text-sm font-light hover:font-semibold hover:text-mid-purple/80"
-              >
-                Home
-              </Link>
-            </div>
-            <div className="lg:flex justify-end max-w-96 w-full h-12 rounded-full border-box border-2 border-gray-400 p-3 lg:text-sm hidden items-center overflow-hidden">
-              <MagnifyingGlassIcon className="size-5 fill-gray-400 ml-2" />
-              <input
-                type="text"
-                className="w-full pl-4 pr-4 bg-off-white text-dark-gray focus:outline-none font-poppins"
-                placeholder="Search for a Gig"
-              />
-            </div>
-            <Menu>
-              <MenuButton className="ml-8 justify-end font-semibold">
-                <UserCircleIcon className="lg:size-11 size-8" />
-              </MenuButton>
-              <Transition
-                enter="transition ease-out duration-75"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <MenuItems
-                  anchor="bottom end"
-                  className="w-56 my-2 font-poppins text-sm origin-top-right rounded-lg p-1 font-normal text-logo-purple bg-white border-box border-2 border-off-white"
+            <div className="flex flex-row">
+              <div className="lg:flex justify-end max-w-96 w-full h-12 rounded-full border-box border-2 border-gray-400 p-3 lg:text-sm hidden items-center overflow-hidden">
+                <MagnifyingGlassIcon className="size-5 fill-gray-400 ml-2" />
+                <input
+                  type="text"
+                  className="w-96 pl-4 pr-4 bg-off-white text-dark-gray focus:outline-none font-poppins"
+                  placeholder="Search for a Gig"
+                />
+              </div>
+              <Menu>
+                <MenuButton className="ml-8 justify-end font-semibold">
+                  <UserCircleIcon className="lg:size-11 size-8" />
+                </MenuButton>
+                <Transition
+                  enter="transition ease-out duration-75"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
                 >
-                  <MenuItem>
-                    <div className="group flex w-full font-semibold items-center gap-2 py-3 px-3 border-box border-b-2 border-off-white">
-                      Welcome, Test User!
-                    </div>
-                  </MenuItem>
-                  <MenuItem>
-                    <button className="group flex w-full items-center gap-2 py-3 px-3 hover:bg-off-white hover:font-semibold">
-                      Sign Out
-                    </button>
-                  </MenuItem>
-                </MenuItems>
-              </Transition>
-            </Menu>
+                  <MenuItems
+                    anchor="bottom end"
+                    className="w-56 my-2 font-poppins text-sm origin-top-right rounded-lg p-1 font-normal text-logo-purple bg-white border-box border-2 border-off-white"
+                  >
+                    <MenuItem>
+                      <div className="group flex w-full font-semibold items-center gap-2 py-3 px-3 border-box border-b-2 border-off-white">
+                        Welcome, Test User!
+                      </div>
+                    </MenuItem>
+                    <MenuItem>
+                      <button className="group flex w-full items-center gap-2 py-3 px-3 hover:bg-off-white hover:font-semibold">
+                        Sign Out
+                      </button>
+                    </MenuItem>
+                  </MenuItems>
+                </Transition>
+              </Menu>
+            </div>
           </div>
         </nav>
         <main className="flex-grow">{children}</main>
--- gig-project/app/(landing)/page.js
+++ gig-project/app/(landing)/page.js
@@ -1,7 +1,43 @@
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
     <div>
-      <div>Code the landing page here</div>
+      <div className="flex flex-col max-w-full max-h-full ml-20 mr-20 mt-5">
+        <div className="flex flex-row max-w-full h-20 font-poppins p-5 justify-between">
+          <div className="text-4xl font-extrabold text-logo-purple">
+            <img className="w-36" src="/name.png" alt="Company Logo" />
+          </div>
+          <div className="flex flex-row max-w-full space-x-8 md:text-lg text-sm font-semibold text-logo-purple hover:text-mid-purple/80">
+            <div className="mt-2 max-w-20 content-center font-poppins justify-center">
+              <Link href="/home" className="group transition-all duration-1000">
+                Log In
+                <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 rounded-xl bg-mid-purple/80"></span>
+              </Link>
+            </div>
+            <Link href="/home">
+              <button className="w-28 max-w-28 rounded-lg bg-mid-purple text-off-white font-poppins h-12 hover:bg-mid-purple/90">
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col font-poppins ml-5 mt-24 ">
+          <div className="mb-3 font-semibold text-lg text-gray-500">
+            Simple. Easy. Transformative.
+          </div>
+          <div className="text-6xl font-semibold space-y-2 transition-all ease-in duration-200 text-logo-purple hover:text-mid-purple/85 hover:cursor-default">
+            <div className="flex flex-row space-x-5 items-center">
+              <div>Rediscover What</div>
+            </div>
+            <div className="flex flex-row">
+              <div className="flex flex-row space-x-5">
+                <div>It Means To Gig.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
