fbcdb7f4f95797e65b3cb28fa86e6cd88ee38d8b
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2024-12-05 16:31:35 -0600

    Update page.tsx

--- app/(home)/[event_id]/submission/[submission_id]/page.tsx
+++ app/(home)/[event_id]/submission/[submission_id]/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 
 import React, { useRef, useState, useEffect } from 'react';
-import { MdArrowOutward, MdCalendarMonth } from 'react-icons/md';
-import { FaLink, FaGithub, FaTwitter, FaUser, FaPaperclip } from 'react-icons/fa';
-import { MdFileCopy } from 'react-icons/md';
+import { MdCalendarMonth } from 'react-icons/md';
+import { FaLink, FaGithub, FaPaperclip } from 'react-icons/fa';
 
-import { CompanyInstance } from '../../../../../util/wrapper/instance'
-import { Company, User } from '../../../../../util/wrapper/static';
+import { Company } from '../../../../../util/wrapper/static';
 
 const SubmissionPage = ({ params }) => {
   const { event_id, submission_id } = params;
   const EventId = parseInt(event_id, 10);
   const SubmissionId = parseInt(submission_id, 10);
 
-  // const projectTitle = "EcoTrack";
-  // const submissionDate = "9/10/2024";
-  // const projectImage = "/figma-logo.png";
-  // const otherLinks = ['', ''];
-  // const projectLink = "https://github.com/AkhilKrishna16/portfolio-2024-2025";
-  // const resumeLink = "https://www.akhilkrishnamurthy.com/Akhil_Krishnamurthy_UT_Austin_Resume_2028.pdf";
-  // const videoLink = "https://www.youtube.com/embed/G0eKzU_fV00";
-  // const projectDescription = "EcoTrack is an innovative mobile application designed to empower users to monitor and reduce their carbon footprint. By integrating real-time data from various sources, EcoTrack provides personalized insights and actionable steps for users to make sustainable choices in their daily lives.";
-  // const projectOutlook = "EcoTrack envisions a world where individuals are empowered to make informed and sustainable choices that collectively lead to a healthier planet. Our goal is to create a vibrant community of eco-conscious users who actively contribute to environmental preservation.";
-
-  const [user, setUser] = useState(null)
   const [activeTab, setActiveTab] = useState("overview");
   const [submission, setSubmission] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -33,7 +20,6 @@ const SubmissionPage = ({ params }) => {
   useEffect(() => {
     async function getSubmissionDetails() {
       const loginResult = await Company.get(); 
-      console.log(loginResult)
       if(loginResult) {
         const submissions = await loginResult.getSubmissions(EventId);
         if(submissions) {
@@ -42,9 +28,9 @@ const SubmissionPage = ({ params }) => {
             setSubmission(selected_submission[0]);
             if(selected_submission[0]) {
               console.log(selected_submission[0].project_link)
-              // console.log(selected_submission[0].project_link)
+
                const links = [
-                { icon: <FaGithub />, text: "Project Link", url: selected_submission[0].project_link ? selected_submission[0].project_link : "#"},
+                { icon: <FaGithub />, text: "Github Link", url: selected_submission[0].project_link ? selected_submission[0].project_link : "#"},
                 { icon: <MdCalendarMonth />, text: `Submitted on ${selected_submission[0].submission_time ? toDatetime(selected_submission[0].submission_time) : Date.now()}`},
                 { icon: <FaPaperclip/>, text: "Resume", url: selected_submission[0].resume_link ? selected_submission[0].resume_link : "#"},
               ];
@@ -77,7 +63,7 @@ const SubmissionPage = ({ params }) => {
     return formattedDate + " at " + formattedTime;
   }
 
-  function getEmbedURL(link) {
+  function getEmbedURL(link: any) {
     const url = new URL(link);
     if (url.hostname === 'www.youtube.com' && url.pathname === '/watch') {
       const videoId = url.searchParams.get('v');
@@ -85,24 +71,6 @@ const SubmissionPage = ({ params }) => {
     }
     return link;
   }
-
-  
-
-  // const links = [
-  //   { icon: <FaGithub />, text: "Project Link", url: submission.project_link},
-  //   { icon: <MdCalendarMonth />, text: `Submitted on ${toDatetime(submission.submission_time)}`},
-  //   { icon: <FaPaperclip/>, text: "Resume", url: submission.resume_link },
-  // ];
-
-  // if (submission.additional_links) {
-  //   links.push(
-  //     ...submission.additional_links.map((link: string, index: number) => ({
-  //       icon: <FaLink />, 
-  //       text: Additional Link ${index},      
-  //       url: link,
-  //     }))
-  //   );
-  // }
  
 
   const scrollToCurrent = () => {
@@ -117,10 +85,6 @@ const SubmissionPage = ({ params }) => {
     (submission && <div className="min-h-screen font-poppins">
       <div className="max-w-[1200px] items-center px-6 py-8 bg-white rounded-2xl mt-10 mx-3 xl:mx-auto mb-10">
         <div className="md:items-start mb-10">
-          {/* <div className="flex justify-center w-full">
-            <img src={projectImage} className="h-40 object-contain max-w-full" alt="Project logo"/>
-          </div> */}
-
           <div className='flex flex-col gap-2'>
             <div onClick={scrollToCurrent} className="flex flex-col md:flex-row items-center md:items-end gap-3 cursor-pointer">
               <h2 className="text-4xl tracking-tight font-semibold">{submission.project_name}</h2>
@@ -128,15 +92,6 @@ const SubmissionPage = ({ params }) => {
                 {submission.submission_id}
               </div>
             </div>
-          
-            {/* <div className='flex flex-row items-center gap-2 border-none rounded-md mt-2'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-fill" viewBox="0 0 16 16">
-                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5"/>
-              </svg>
-              <p className='font-normal text-sm'>Submitted on <span className='text-purple-500'>10/15/2022</span></p>
-            </div> */}
-            
-            
           </div>
         </div>
 
@@ -154,10 +109,6 @@ const SubmissionPage = ({ params }) => {
 
         <div className="pt-8 border-t border-gray-300 w-full" />
 
-        
-        {/* <h1 className="text-2xl mb-4 mt-4">About</h1> */}
-
-        {/* Tab navigation */}
         <div className="flex space-x-4 mt-4 border-gray-300">
           <button 
             onClick={() => setActiveTab("overview")} 
@@ -173,9 +124,6 @@ const SubmissionPage = ({ params }) => {
           </button>
         </div>
 
-        
-        
-        {/* Tab content */}
         <div className="mt-8">
           {activeTab === "overview" && (
             <>
